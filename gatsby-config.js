f4e6aeffad37faac7239df5edd851238a459b3f2
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-12-16 23:18:55 -0500

    adding support for sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lenny Gonzalez`,
+    description: `You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.`,
+    author: `@elCapitan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,`gatsby-plugin-sass`,
