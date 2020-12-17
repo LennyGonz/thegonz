02207537ae9c8d1a5f48c53c84086946dbfed809
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-12-16 23:19:55 -0500

    creating the navbar component and its styling

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// import { FaUserAstronaut } from 'react-icons/fa'
+import { FaUserAstronaut } from "@react-icons/all-files/fa/FaUserAstronaut"
 import '../../styles/navbar.scss';
 
 class NavBar extends Component {
@@ -14,21 +16,25 @@ class NavBar extends Component {
 
           <li className="nav-item">
             <a>
+              
               HOME
             </a>
           </li>
           <li className="nav-item">
             <a>
+              <FaUserAstronaut className="rotate" />
               ABOUT
             </a>
           </li>
           <li className="nav-item">
             <a>
+              
               SKILLS
             </a>
           </li>
           <li className="nav-item">
             <a>
+              
               PROJECTS
             </a>
           </li>
