37157d9343799e6ba93cc7fc4d8d098d109d9a34
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-12-17 23:39:52 -0500

    continuing to tweak the nav

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,45 +1,43 @@
 import React, { Component } from 'react';
-// import { FaUserAstronaut } from 'react-icons/fa'
-import { FaUserAstronaut } from "@react-icons/all-files/fa/FaUserAstronaut"
+import { FaUserAstronaut } from "@react-icons/all-files/fa/FaUserAstronaut";
+import { IoFolderOpenSharp, IoHome } from "react-icons/io5";
+import { GiGearHammer } from "@react-icons/all-files/gi/GiGearHammer";
+import { SiLinkedin, SiGithub } from "react-icons/si";
+import LogoImg from '../../images/teddy-bear.png';
 import '../../styles/navbar.scss';
 
 class NavBar extends Component {
   render() {
     return (
-      <div>
-        <nav>
-          <li className="logo">
-            <a>
-
-            </a>
-          </li>
-
-          <li className="nav-item">
-            <a>
-              
-              HOME
-            </a>
-          </li>
-          <li className="nav-item">
-            <a>
-              <FaUserAstronaut className="rotate" />
-              ABOUT
-            </a>
-          </li>
-          <li className="nav-item">
-            <a>
-              
-              SKILLS
-            </a>
-          </li>
-          <li className="nav-item">
-            <a>
-              
-              PROJECTS
-            </a>
-          </li>
-        </nav>
-      </div>
+      <nav className="nav">
+        <div className="navbar-container">
+          <div className="logoContainer">
+            <img className="logo" src={LogoImg} alt="logo" />
+          </div>
+          <ul className="navmenu">
+            <nav className="nav-item nav-item__home">
+              <IoHome />
+            </nav>
+            <nav className="nav-item nav-item__about">
+              <FaUserAstronaut />
+            </nav>
+            <nav className="nav-item nav-item__skills">
+              <GiGearHammer />
+            </nav>
+            <nav className="nav-item nav-item__projects">
+              <IoFolderOpenSharp />
+            </nav>
+          </ul>
+          <div className="linksmenu">
+            <div className="link-item link-item__linkedin">
+              <SiLinkedin />
+            </div>
+            <div className="link-item link-item__github">
+              <SiGithub />
+            </div>
+          </div>
+        </div>
+      </nav>
     )
   }
 }
