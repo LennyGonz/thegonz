257f2a5918a0af94ea35d4ae40013b44a68e1fe2
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-12-18 23:39:41 -0500

    need to take the mobile nav into acconut

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,23 +7,11 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import NavBar from "./navbar"
 import "../styles/layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <div className="grid-container">
