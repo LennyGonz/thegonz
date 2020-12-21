81920ea733eabdb9303dac873daa64097f601be1
lennygonz <lenny.gonzaleznyc@gmail.com>
2020-12-20 19:16:49 -0500

    adjustments to the nav

--- src/components/mobile/index.js
+++ src/components/mobile/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { FaUserAstronaut } from "@react-icons/all-files/fa/FaUserAstronaut";
+import { IoFolderOpenSharp, IoHome } from "react-icons/io5";
+import { GiGearHammer } from "@react-icons/all-files/gi/GiGearHammer";
+import { SiLinkedin } from "react-icons/si";
+import '../../styles/mobileNav.scss';
 
-const mobileNav = () => {
+
+const MobileNav = () => {
+  const toggle = () => {
+    let x = document.getElementByClassName("nav-mobile")
+    
+  }
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <>
+      <nav className="nav-mobile">
+        <ul className="navmenu-mobile">
+          <li className="nav-item-mobile nav-item-mobile__home">
+            <IoHome />
+          </li>
+          <li className="nav-item-mobile nav-item-mobile__about">
+            <FaUserAstronaut />
+          </li>
+          <li className="nav-item-mobile nav-item-mobile__skills">
+            <GiGearHammer />
+          </li>
+          <li className="nav-item-mobile nav-item-mobile__projects">
+            <IoFolderOpenSharp />
+          </li>
+          <li className="nav-item-mobile nav-item-mobile__linkedin">
+            <SiLinkedin />
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
+
+export default MobileNav;
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FaUserAstronaut } from "@react-icons/all-files/fa/FaUserAstronaut";
-import { IoFolderOpenSharp, IoHome } from "react-icons/io5";
+import { IoFolderOpenSharp, IoHome, IoMenu } from "react-icons/io5";
 import { GiGearHammer } from "@react-icons/all-files/gi/GiGearHammer";
 import { SiLinkedin, SiGithub } from "react-icons/si";
 import LogoImg from '../../images/teddy-bear.png';
@@ -10,32 +10,33 @@ class NavBar extends Component {
   render() {
     return (
       <nav className="nav">
-        <div className="navbar-container">
-          <div className="logoContainer">
-            <img className="logo" src={LogoImg} alt="logo" />
-          </div>
-          <ul className="navmenu">
-            <li className="nav-item nav-item__home">
-              <IoHome />
-            </li>
-            <li className="nav-item nav-item__about">
-              <FaUserAstronaut />
-            </li>
-            <li className="nav-item nav-item__skills">
-              <GiGearHammer />
-            </li>
-            <li className="nav-item nav-item__projects">
-              <IoFolderOpenSharp />
-            </li>
-          </ul>
-          <div className="linksmenu">
-            <div className="link-item link-item__linkedin">
-              <SiLinkedin />
-            </div>
-            <div className="link-item link-item__github">
-              <SiGithub />
-            </div>
-          </div>
+        <div className="logoContainer">
+          <img className="logo" src={LogoImg} alt="logo" />
+        </div>
+        <ul className="navmenu">
+          <li className="nav-item nav-item__home">
+            <IoHome />
+          </li>
+          <li className="nav-item nav-item__about">
+            <FaUserAstronaut />
+          </li>
+          <li className="nav-item nav-item__skills">
+            <GiGearHammer />
+          </li>
+          <li className="nav-item nav-item__projects">
+            <IoFolderOpenSharp />
+          </li>
+        </ul>
+        <ul className="linksmenu">
+          <li className="link-item link-item__linkedin">
+            <SiLinkedin />
+          </li>
+          <li className="link-item link-item__github">
+            <SiGithub />
+          </li>
+        </ul>
+        <div className="mobile-view">
+          <IoMenu />
         </div>
       </nav>
     )
