46bdc0c377b45a35d12ec41a1378ff966c67f181
lennygonz <lenny.gonzaleznyc@gmail.com>
2021-01-03 23:33:52 -0500

    component is good to go

--- src/components/mobile/index.js
+++ src/components/mobile/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Nav, NavMenuMobile, ListItem, HomeBtn, AboutBtn, SkillsBtn, ProjectsBtn, LinkedIn } from './mobileNavBarElements';
+import { Nav, NavMenuMobile, ListItem, LinkItem, HomeBtn, AboutBtn, SkillsBtn, ProjectsBtn, LinkedIn } from './mobileNavBarElements';
 
 const MobileNav = ({ open }) => {
   return (
     <div>
       <Nav open={open}>
         <NavMenuMobile>
-          <ListItem>
+          <ListItem to="/" activeClassName="active">
             <HomeBtn />
           </ListItem>
-          <ListItem>
+          <ListItem to="/about" activeClassName="active">
             <AboutBtn />
           </ListItem>
-          <ListItem>
+          <ListItem to="/skills" activeClassName="active">
             <SkillsBtn />
           </ListItem>
-          <ListItem>
+          <ListItem to="/projects" activeClassName="active">
             <ProjectsBtn />
           </ListItem>
-          <ListItem>
-            <LinkedIn />
-          </ListItem>
+          <LinkItem>
+            <a href="https://www.linkedin.com/in/lenny-gonzalez">
+              <LinkedIn />    
+            </a>
+          </LinkItem>
         </NavMenuMobile>
       </Nav>
     </div>
