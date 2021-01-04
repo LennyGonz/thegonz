e8d72007110d49bb46bab6ed827286a43312411e
lennygonz <lenny.gonzaleznyc@gmail.com>
2021-01-03 23:34:07 -0500

    styling needs to be tweaked

--- src/components/mobile/mobileNavBarElements.js
+++ src/components/mobile/mobileNavBarElements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'gatsby';
 import { FaUserAstronaut } from "@react-icons/all-files/fa/FaUserAstronaut";
 import { IoFolderOpenSharp, IoHome } from "react-icons/io5";
 import { GiGearHammer } from "@react-icons/all-files/gi/GiGearHammer";
@@ -17,7 +18,7 @@ export const Nav = styled.nav`
     top: 120px;
     transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
     transition-property: opacity, right;
-    transition: 0.4s ease-in-out;
+    transition: all 0.4s ease-in-out;
   }
 `;
 export const NavMenuMobile = styled.ul`
@@ -33,7 +34,24 @@ export const NavMenuMobile = styled.ul`
     top: 120px;
   }
 `;
-export const ListItem = styled.li`
+export const ListItem = styled(Link)`
+  display: none;
+  @media screen and (min-width: 360px) and (max-width: 896px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    top: 120px;
+
+    &.active {
+      border-bottom: 3px solid #01bf71;
+    }
+  }
+`;
+
+export const LinkItem = styled.div`
   display: none;
   @media screen and (min-width: 360px) and (max-width: 896px) {
     display: flex;
